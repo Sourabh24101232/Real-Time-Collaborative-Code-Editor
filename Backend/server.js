97a805bf87ef03c922e6ce245f26781cb1e4fc76
Sourabh Kumar <sourabhkumar920652@gmail.com>
2026-06-30 09:16:43 +0530

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+//Used to create an HTTP server.
+import { createServer } from "http"
+//Imports the Server class from Socket.IO. This class creates a WebSocket server.
+import { Server } from "socket.io"
+//YSocketIO connects Yjs  with Socket.IO.
+import { YSocketIO } from "y-socket.io/dist/server"
+
